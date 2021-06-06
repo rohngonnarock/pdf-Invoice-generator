5b324478880899720deb6a982a8677f1df12316c
rahul prajapati <rohngonnarock@gmail.com>
2021-06-06 09:48:05 +0530

    primeng added

--- src/app/add-invoice/add-invoice.component.ts
+++ src/app/add-invoice/add-invoice.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from '../product';
+import { SelectItem } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { ProductService } from '../product.service';
 @Component({
   selector: 'app-add-invoice',
+  providers: [MessageService],
   templateUrl: './add-invoice.component.html',
-  styleUrls: ['./add-invoice.component.scss']
+  styleUrls: ['./add-invoice.component.scss'],
 })
 export class AddInvoiceComponent implements OnInit {
+  products1: Product[] = [];
 
-  constructor() { }
+  products2: Product[] = [];
 
-  ngOnInit(): void {
+  statuses: SelectItem[] = [];
+
+  clonedProducts: { [s: string]: Product } = {};
+
+  constructor(
+    private productService: ProductService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit() {
+    this.productService
+      .getProductsSmall()
+      .then((data) => (this.products1 = data));
+    this.productService
+      .getProductsSmall()
+      .then((data) => (this.products2 = data));
+
+    this.statuses = [
+      { label: 'In Stock', value: 'INSTOCK' },
+      { label: 'Low Stock', value: 'LOWSTOCK' },
+      { label: 'Out of Stock', value: 'OUTOFSTOCK' },
+    ];
   }
 
+  onRowEditInit(product: Product = {}) {
+    this.clonedProducts[product.id!] = { ...product };
+  }
+
+  onRowEditSave(product: Product) {
+    if (product.price! > 0) {
+      delete this.clonedProducts[product.id!];
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Success',
+        detail: 'Product is updated',
+      });
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Invalid Price',
+      });
+    }
+  }
+
+  adRow() {
+    const newObj = {} as Product;
+    this.products2.push(newObj);
+  }
+
+  onRowEditCancel(product: Product, index: number) {
+    this.products2[index] = this.clonedProducts[product.id!];
+    delete this.products2[Number(product.id!)];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddInvoiceComponent } from './add-invoice/add-invoice.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { CalendarModule } from 'primeng/calendar';
+import { SliderModule } from 'primeng/slider';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from './product.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AddInvoiceComponent,
-    DashboardComponent
-  ],
+  declarations: [AppComponent, AddInvoiceComponent, DashboardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    TableModule,
+    CalendarModule,
+    SliderModule,
+    DialogModule,
+    MultiSelectModule,
+    ContextMenuModule,
+    DropdownModule,
+    ButtonModule,
+    ToastModule,
+    InputTextModule,
+    ProgressBarModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ProductService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
